e1d9cdce62f4e3b8a0d534b8953ee3da34ccd57c
Levi Starrett <levi@roxsoftware.com>
2024-11-21 13:05:45 -0500

    fixup

--- freeze.py
+++ freeze.py
@@ -1,7 +1,9 @@
 from py2exe import freeze
 
 freeze(console=[{'script': 'rsl/gen_erate.py'}],
-    packages=['xtuml', 'rsl'],
-    compressed=1,
-    optimize=2,
-    bundle_files=1)
+    options={
+       'packages': ['xtuml', 'rsl'],
+       'compressed': 1,
+       'optimize': 2,
+       'bundle_files': 1
+    })
