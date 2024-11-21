b2b9e3d2f7508d0ff3f3bd9961e9521901951724
Levi Starrett <levi@roxsoftware.com>
2024-11-21 13:02:04 -0500

    fix CI

--- freeze.py
+++ freeze.py
@@ -1,2 +1,7 @@
 from py2exe import freeze
-freeze(console=[{'script': 'rsl/gen_erate.py'}])
+
+freeze(console=[{'script': 'rsl/gen_erate.py'}],
+    packages=['xtuml', 'rsl'],
+    compressed=1,
+    optimize=2,
+    bundle_files=1)
