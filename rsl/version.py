19520e2fe5477ea6574a1bc6741dfda98e93cde0
Levi Starrett <levi@roxsoftware.com>
2024-11-27 13:58:30 -0500

    Bump version to 3.0.0

--- rsl/version.py
+++ rsl/version.py
@@ -6,8 +6,8 @@ Keep version info in one single place
 
 
 name = 'pyrsl'
-date = '2021-11-06'
-version = '2.1'
-release = '2.1.1' #  # ensure that this is the same as in setup.py
+date = '2024-12-02'
+version = '3.0'
+release = '3.0.0' #  # ensure that this is the same as in setup.py
 
 complete_string = '%s v%s (%s)' % (name, release, date)
